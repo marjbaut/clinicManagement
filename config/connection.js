2cbc9383949bfe475e7d23ce99a33fc6afdaadf3
August Tornea <august.tornea@gmail.com>
2023-05-16 10:24:09 -0400

    added patient controller

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
-
 let sequelize;
 //this if statement will check the environment in which the server is about to run. if heroku then it wont try to run on localhost otherwise go down ))
 if (process.env.JAWSDB_URL) {
@@ -10,12 +9,14 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    
     {
-      host: 'localhost',
+      host: '172.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
+    
   );
-}
+}console.log(process.env.DB_PASSWORD)
 
 module.exports = sequelize;
