c4f347a312b265d8dd970992e20f875dd32c5bb8
August Tornea <august.tornea@gmail.com>
2023-05-16 11:16:42 -0400

    added error handling seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,41 +1,37 @@
 const sequelize = require('../config/connection');
-const {Specialty, MedicalStaff, Patient, Appointment}= require('../models');
+const { Specialty, MedicalStaff, Patient, Appointment } = require('../models');
 
 const specialtyData = require('./specialtyData.json');
 const medicalStaffData = require('./medicalStaffData.json');
 const patientData = require('./patientData.json');
 const appointmentData = require('./appointmentData.json');
 
-
 const seedDatabase = async () => {
+  try {
     await sequelize.sync({ force: true });
 
+    for (const specialty of specialtyData) {
+      await Specialty.create({ ...specialty });
+    }
 
-    for (const specialty of specialtyData){
-        await Specialty.create({
-            ...specialty,
-        })
-    };
+    for (const medicalStaff of medicalStaffData) {
+      await MedicalStaff.create({ ...medicalStaff });
+    }
 
-    for (const medicalstaff of medicalStaffData){
-        await MedicalStaff.create({
-            ...medicalstaff,
-        })
-    };
+    for (const patient of patientData) {
+      await Patient.create({ ...patient });
+    }
 
-    for (const patient of patientData){
-        await Patient.create({
-            ...patient,
-        })
-    };
+    for (const appointment of appointmentData) {
+      await Appointment.create({ ...appointment });
+    }
 
-    for (const appointment of appointmentData){
-        await Appointment.create({
-            ...appointment,
-        })
-    };
-    
-    process.exit(0)
+    console.log('Database seeded successfully');
+    process.exit(0);
+  } catch (error) {
+    console.error('Error seeding database:', error);
+    process.exit(1);
+  }
 };
 
 seedDatabase();
