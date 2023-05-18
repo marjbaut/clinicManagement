8309d7b4319a2dbc5cc9e25b1a6ff22623e0d48a
Marjorie <marjoriebaut.w@gmail.com>
2023-05-18 15:44:57 -0400

    a

--- controllers/patientList/index.js
+++ controllers/patientList/index.js
@@ -24,26 +24,28 @@ router.get('/:patient_id', async (req, res) => {
   res.render('patientChart', nicePatientData);
 });
 
-// router.put('/:patient_id', async (req, res) => {
-//   const patientId = req.params.patient_id;
-//   const result = await Patient.update({patient_id:patientId },res.body);
-//   console.log(result);
-// });
+router.put('/:patient_id', async (req, res) => {
+  const patientId = req.params.patient_id;
+  const result = await Patient.update({patient_id:patientId },res.body);
+  console.log(result);
+});
 
-// router.put('/:patient_id', async (req, res) => {
-//   try {
-//     const noteData = await Patient.update(req.body,{
-//       where: {
-//         patient_id: req.params.patient_id
-//       }
-//     });
-//     res.status(200).json(noteData);
-//     console.log(noteData);
-
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
+router.put('/patientList/:patient_id', async (req, res) => {
+  try {
+    const { patient_id } = req.params;
+    const { doctorNote } = req.body;
+    console.log("do we see id", patient_id);
+    console.log('what is doctorNOte', req.body);
+    const updatedPatient = await Patient.update(
+      { medical_notes: doctorNote },
+      { where: { id: patient_id } } // Use 'id' instead of 'patient_id'
+    );
+    res.status(200).json(updatedPatient);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update medical notes.' });
+  }
+});
+module.exports = router;
 
 
 router.post('/', async (req, res) => {
--- public/js/index.js
+++ public/js/index.js
@@ -1,60 +1,38 @@
-// const noteForm = document.getElementById('noteForm');
-
-// console.log(noteForm);
-
-// const noteSubmission = (e) => {
-//     e.preventDefault();
-//     const doctorNoteValue = document.getElementById('doctorNoteInput').value;
-//     console.log(doctorNoteValue);
-
-//     const saveNote = (note) =>
-//         fetch('/patientList/:patient_id', {
-//             method: 'PUT',
-//             body
-//         });
-//     saveNote(doctorNoteValue);
-
-// }
-// noteForm.addEventListener('submit', noteSubmission);
-
-// const getNotes = () =>
-//     fetch('/patientList/patient_id', {
-//         method: 'GET',
-//     });
-
-// // const saveNote = (note) =>
-// //   fetch('/patientList/:patient_id', {
-// //     method: 'PUT',
-
-// //   });
-
-
-
-
-// console.log('hello')
-
-// const noteForm = document.getElementById('noteForm')
-
-// const formSubmission = async (e) => {
-//     e.preventDefault();
-
-//     const noteValue = document.getElementById('doctorNoteInput').value;
-//     console.log(noteValue);
-
-//     const putValue = await fetch('/patientList/:patient_id', {
-//         method: "PUT",
-//         body: JSON.stringify({
-//             doctorNote: noteValue,
-//         }),
-//         headers: { 'Content-Type': 'application/json' },
-//     })
-
-//     if(putValue.ok) {
-//         alert("Your update is up!");
-//     } else {
-//         alert('Go fix your codeeeee');
-//     }
-// }
+const formSubmission = async (e, patientId) => {
+    e.preventDefault();
+ 
+    const noteValue = document.getElementById('doctorNoteInput').value;
+    console.log(noteValue);
+    console.log("from UI", patientId);
+    const url = `/patientList/patientList/${patientId}`;
+ 
+    const putValue = await fetch(url, {
+      method: "PUT",
+      body: JSON.stringify({
+        doctorNote: noteValue
+      }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then(res => {
+        if(res.ok) {
+            return res.json();
+        } else {
+            alert("Go fix your code!");
+        }
+       })
+    .then(data => console.log(data))
+    .catch(err => console.log("this is problem", err));
+  };
+  const submitButtons = document.querySelectorAll('.btn.btn-primary[data-patient-id]');
+  submitButtons.forEach(button => {
+    const patientId = button.dataset.patientId;
+    console.log(patientId)
+    button.addEventListener('click', (e) => {
+      formSubmission(e, patientId);
+    });
+  });
 
 
-// noteForm.addEventListener('submit', formSubmission);
+
+
+
+
